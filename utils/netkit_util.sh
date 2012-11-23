3937d95b9a7bd68a4d535aabc2fdfcc1dab69159
vheon <v.vheon@gmail.com>
2012-11-23 02:20:45 +0100

    Add some snippets and some utility

--- utils/netkit_util.sh
+++ utils/netkit_util.sh
@@ -1,1 +1,26 @@
 #!/usr/bin/env bash
+
+hw()
+{
+    local hws="$@"
+
+    for m in $hws; do
+        mkdir $m
+        touch $m.startup
+    done
+}
+
+
+bindit()
+{
+    local hws="$@"
+
+    for m in $hws; do
+        if [[ -f $m.startup ]] && [[ -d $m ]]; then
+            echo "" >> $m.startup
+            echo "/etc/init.d/bind start" >> $m.startup
+            mkdir -p $m/etc/bind
+            touch $m/etc/bind/named.conf
+        fi
+    done
+}

a99e01c1c533a0ce122790593784d78ea4d74655
Andrea Cedraro <v.vheon@gmail.com>
2012-11-23 13:56:19 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,17 @@ ln -s $dir/vim $HOME/.vim
 ln -s $dir/vim/vimrc $HOME/.vimrc
 ln -s $dir/bin $HOME/bin
 
+echo
 echo "loading xmodmap config"
 xmodmap ~/.Xmodmap
 
+echo
 echo "setting solarized in gnome"
 sh $dir/utils/gnome-terminal-colors-solarized-master/set_dark.sh
 
+echo
+echo "add \$HOME/bin to PATH"
+echo "export PATH=$HOME/bin:$PATH" >> $HOME/.bashrc
+echo
+echo "starting simpleswitcher"
+simpleswitcher &> /dev/null &

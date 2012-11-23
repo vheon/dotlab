2e63e152f7395aeadae1b6819971747630a9b575
Andrea Cedraro <v.vheon@gmail.com>
2012-11-23 12:54:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ dir=$(basename $0)
 echo "symlinking vim, bin, xmodmap"
 ln -s $dir/Xmodmap $HOME/.Xmodmap
 ln -s $dir/vim $HOME/.vim
-ln -s $dir/vim/.vimrc $HOME/.vimrc
+ln -s $dir/vim/vimrc $HOME/.vimrc
 ln -s $dir/bin $HOME/bin
 
 echo "loading xmodmap config"
 xmodmap ~/.Xmodmap
 
 echo "setting solarized in gnome"
-sh $dir/utils/gnome-terminal-colors-colarized-master/set_dark.sh
+sh $dir/utils/gnome-terminal-colors-solarized-master/set_dark.sh
 
